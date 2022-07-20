fe1e72239835c4f768d06867b4493eb408d4fe52
GribkovBoris <bbg94q@gmail.com>
2022-07-20 20:36:52 +0300

    Add some code

---
+++ test.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                          options=chrome_options)
+
+driver.get('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox')
+if(False):
+    searchbox = driver.find_element(By.XPATH, '//*[@id="search"]/input')
+    searchbox.send_keys('kek')
+    button = driver.find_element(By.XPATH, '//*[@id="search"]/span/button')
+    button.click()
+
+#driver.quit()
