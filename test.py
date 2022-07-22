25c0004be8821ff6df550a2a0ec1fea83fb7b3dd
GribkovBoris <bbg94q@gmail.com>
2022-07-22 15:25:57 +0300

    new

--- test.py
+++ test.py
@@ -3,14 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
 
-# chrome_options.add_experimental_option("detach", True)
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                          options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 def visit_site_for_label():
--- tester.py
+++ tester.py
@@ -1,9 +1,10 @@
 from test import visit_site_for_label
 
-print("After import")
-if(False):
-    visit_site_for_label()
-# def test_visit():
-#    assert visit_site_for_label() == "Your Store"
 
-print("Tester here")
+def test_visit():
+    text = visit_site_for_label()
+    print(text)
+    assert text == "Your Store"
+
+
+test_visit()
